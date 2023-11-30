1e98a1ab8ddaa61132a76b4c41375bc436b22a96
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2023-11-30 20:41:29 +0300

    Додано файл README та приклад роботи додатку

--- src/main/java/com/malikov/Main.java
+++ src/main/java/com/malikov/Main.java
@@ -2,6 +2,22 @@ package com.malikov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product prod1 = new Product(0, "EnergyDrink", 11.11);
+        Product prod2 = new Product(1, "Apple", 5.99);
+        Product prod3 = new Product(2, "Pineaple", 26.56);
+
+
+        Cart cart = new Cart();
+        cart.addProd(prod1, prod2, prod3);
+
+
+        Order order = new Order(0, cart);
+        System.out.println(order.getStatus());
+
+        order.PlaceOrder();
+        System.out.println(order.getStatus());
+        
+        order.ProcessOrder();
+        System.out.println(order.getStatus());
     }
 }
